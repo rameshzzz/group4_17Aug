5f7e7518f66758c13dc758b1bb73aaab9dd90bd5
Jainam Gandhi <jainam0701@gmail.com>
2019-08-17 16:51:14 +0530

    boxplot

---
+++ boxplot.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+if __name__ == “__main__”:
+    array =  np.random.randint(0,100,20).reshape(2,10)
+    print(array)
+    plt.boxplot(array)  
+    plt.show()
