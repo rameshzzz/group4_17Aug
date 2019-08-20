d29b1d20b3ae0725f9da3e3f669742df1c02cc44
Girish Vankudre <girish.vankudre@gmail.com>
2019-08-20 11:42:36 +0530

    boxplot file added with Task2

--- boxplot.py
+++ boxplot.py
@@ -3,9 +3,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-if __name__ == “__main__”:
+# Task 1
+if __name__ == "__main__":
     array =  np.random.randint(0,100,20).reshape(2,10)
     print(array)
     plt.boxplot(array)  
     plt.show()
-# hi comment
+
+# Task 2
+if __name__ == "__main__":
+    print ("Mean is: ",np.mean(array))
+    print ("Median is: ",np.median(array))
+    print ("25th Percentile: ",np.percentile(array,25))
+    print ("75th Percentile: ",np.percentile(array,75))
